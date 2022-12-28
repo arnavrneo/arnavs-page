32ca807a9b847a229dc13043eec4b1dbbc069426
arnavrneo <arnavraina02@gmail.com>
2022-12-28 21:56:51 +0530

    Files

---
+++ astro.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import tailwind from "@astrojs/tailwind";
+import image from "@astrojs/image";
+import sitemap from "@astrojs/sitemap";
+import prefetch from "@astrojs/prefetch";
+
+// https://astro.build/config
+export default defineConfig({
+	site: "https://arnavr.pages.dev",
+	markdown: {
+		shikiConfig: {
+			theme: "material-darker",
+			wrap: true,
+		},
+	},
+	integrations: [
+		mdx({}),
+		tailwind({
+			config: {
+				applyBaseStyles: false,
+			},
+		}),
+		image({
+			serviceEntryPoint: "@astrojs/image/sharp",
+		}),
+		sitemap(),
+		prefetch(),
+	],
+});
---
+++ src/site.config.ts
@@ -0,0 +1,10 @@
+const siteMeta = {
+	title: "Arnav's Page",
+	lang: "en-GB",
+	ogLocale: "en_GB",
+	githubUrl: "https://github.com/arnavrneo/arnavs-pages",
+	themeColorLight: "#fafafa",
+	themeColorDark: "#1d1f21",
+};
+
+export default siteMeta;
